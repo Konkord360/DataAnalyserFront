3c41c1bdb8f394bd5c30c9cf41beac1d88a9e767
Konkord360 <Konkord720@gmail.com>
2021-01-03 18:48:12 +0100

    deleted unused dependencied

--- src/FileUpload.js
+++ src/FileUpload.js
@@ -1,12 +1,40 @@
-import { render } from "@testing-library/react";
-import React, { Fragment, useEffect, useRef, useState } from "react";
-import "./App.css";
-import axios, { post } from "axios";
-import "react-dropzone-uploader/dist/styles.css";
-import Dropzone, { Preview } from "react-dropzone-uploader";
-import { color } from "highcharts";
 
+import React, { useState, useMemo } from "react";
+import "./App.css";
+import "react-dropzone-uploader/dist/styles.css";
+import {useDropzone} from 'react-dropzone';
+import Button from "@material-ui/core/Button";
+
+const baseStyle = {
+  flex: 1,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: '20px',
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: 'black',
+  borderStyle: 'solid',
+  backgroundColor: '#2b3033',
+  color: '#1890ff',
+  outline: 'none',
+  transition: 'border .24s ease-in-out'
+};
+
+const activeStyle = {
+  borderColor: '#2196f3'
+};
+
+const acceptStyle = {
+  borderColor: '#00e676'
+};
+
+const rejectStyle = {
+  borderColor: '#ff1744'
+};
 const FileUpload = ({ refetch }) => {
+  const [filteredFiles, setFilteredFiles] = useState();
+
   // const Preview = ({ meta }) => {
   //     const { name, percent, status } = meta
   //     return (
@@ -17,6 +45,7 @@ const FileUpload = ({ refetch }) => {
   //   }
   // setHidden(isHidden);
   const [selectedFile, setSelectedFile] = useState("");
+  // const [fileList, setFileList] = useState();
 
   const onFormSubmit = (e) => {
     e.preventDefault();
@@ -60,16 +89,62 @@ const FileUpload = ({ refetch }) => {
         f.remove();
       })
     );
-    // allFiles.forEach(
-    //     f => f.remove()
-    // )
   };
 
-  const dropzoneStyle = {
-    width: "100%",
-    height: "20%",
-    border: "1px solid black",
+  // const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
+  const {
+    acceptedFiles,
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject
+  } = useDropzone(); 
+
+  
+  const files = acceptedFiles.map((file, i) => (
+    <div className="filePreview">
+      <div className="fileName">
+        <li key={file.path}>
+          {file.path} - {file.size} bytes
+        </li>
+      </div>
+        <div className="filePreviewButton">
+          <Button variant="outlined" color="primary" onClick={() => remove(i)}>
+            delete file
+          </Button>
+        </div>
+      
+    </div>
+  ));
+
+  const remove = (file) => {
+    acceptedFiles.splice(file, 1);        // remove the file from the array
+    files.splice(file, 1); 
+    setFilteredFiles(files);
   };
+
+  const style = useMemo(() => ({
+    ...baseStyle,
+    ...(isDragActive ? activeStyle : {}),
+    ...(isDragAccept ? acceptStyle : {}),
+    ...(isDragReject ? rejectStyle : {})
+  }), [
+    isDragActive,
+    isDragReject,
+    isDragAccept
+  ]);
+
+  const uploadFiles = () => {
+    console.log(acceptedFiles);
+    console.log(files);
+    if(files.length > 0){
+      for(let i = 0; i < files.length; i++){
+        fileUpload(acceptedFiles[i]);
+        remove(i);
+      }
+    }
+  }
 
   return (
     // <div>
@@ -79,22 +154,39 @@ const FileUpload = ({ refetch }) => {
     //         <button type="submit">Upload</button>
     //     </form>
     // </div>
-    <div className="DropZoneDiv">
-      <Dropzone
-        // getUploadParams={getUploadParams}
-        onChangeStatus={handleChangeStatus}
-        onSubmit={handleSubmit}
-        canCancel={true}
-        inputContent="Drop files here"
-        styles={{
-          dropzone: {
-            minHeight: 200,
-            maxHeight: 600,
-            backgroundColor: "white",
-            borderColor: "#1890ff",
-          },
-        }}
-      ></Dropzone>
+    // <div className="DropZoneDiv">
+    //   <Dropzone
+    //     // getUploadParams={getUploadParams}
+    //     onChangeStatus={handleChangeStatus}
+    //     onSubmit={handleSubmit}
+    //     canCancel={true}
+    //     Layout={Layout}
+    //     // PreviewComponent={Preview}
+    //     inputContent="Drop files here"
+    //     styles={{
+    //       dropzone: {
+    //         minHeight: 200,
+    //         maxHeight: 600,
+    //         backgroundColor: "#2b3033",
+    //         borderColor: "#1890ff",
+    //       },
+    //     }}
+    //   ></Dropzone>
+    // </div>
+    <div className="DropzoneContainer">
+      <div {...getRootProps({style})}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
+      <div className="filesPreview">
+        <h4>Files</h4>
+        {files}
+      </div>
+      <div className="SubmitButton">
+          <Button variant="outlined" color="primary" onClick={uploadFiles}>
+            Upload files
+          </Button>
+      </div>
     </div>
   );
 };
